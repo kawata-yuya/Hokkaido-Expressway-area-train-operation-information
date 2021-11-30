c4597d912c97c19c2e763ffe7d045187c1a46fd1
kawata-yuya <kawatayuya@icloud.com>
2021-11-30 22:58:03 +0900

    fix 情報取得先の変更

--- muroran_status.py
+++ muroran_status.py
@@ -4,7 +4,7 @@ import bs4
 UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) '\
     'AppleWebKit/537.36 (KHTML, like Gecko) '\
         'Chrome/55.0.2883.95 Safari/537.36 '
-url = "https://www3.jrhokkaido.co.jp/webunkou/?a=2"
+url = "https://www3.jrhokkaido.co.jp/webunkou/data/AREA_CENTRAL_DATA.json"
 # url = "https://web.archive.org/web/20201218070004/https://www3.jrhokkaido.co.jp/webunkou/?a=2"
 
 
@@ -14,17 +14,18 @@ def status_getter():
     r.encoding = r.apparent_encoding
 
 
-    soup = bs4.BeautifulSoup(r.text, features="html.parser")
+    #soup = bs4.BeautifulSoup(r.text, features="html.parser")
+    #
+    #for br in soup.find_all("br"):
+    #    br.replace_with("\n")
+    #
+    #data = soup.find('div', class_="mainbodyInner").text
+    #
+    #
+    #text = data.replace('\n\n\n', '\n\n')
 
-    for br in soup.find_all("br"):
-        br.replace_with("\n")
-
-    data = soup.find('div', class_="mainbodyInner").text
-    
-
-    text = data.replace('\n\n\n', '\n\n')
-
-    return text + url
+    return r.text.replace('<BR>', '\n') + \
+        "https://www3.jrhokkaido.co.jp/webunkou/?a=2"
 
 
 if __name__ == '__main__':
